1fcd88eef52bbc5242eff95e9d5fc43030bc7ae2
Flávio Dutra <flaviomdutra@outlook.com>
2024-02-24 13:48:11 -0300

    Initial commit

---
+++ internal/entity/client_test.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("John Doe", "jonh.doe@email.com")
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "jonh.doe@email.com", client.Email)
+	assert.NotEmpty(t, client.ID)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "jonh.doe@email.com")
+	err := client.Update("Jane Doe", "jane.doe@email.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "Jane Doe", client.Name)
+	assert.Equal(t, "jane.doe@email.com", client.Email)
+}
+
+func TestUpdateClientWhenArgsAreInvalid(t *testing.T) {
+	client, _ := NewClient("John Doe", "jonh.doe@email.com")
+	err := client.Update("", "jane.doe@email.com")
+	assert.Error(t, err, "name is required")
+}
