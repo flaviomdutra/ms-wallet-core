0e309389b2c03ae245fc8c897d5b7db84c629b38
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-01 00:04:49 -0300

    Add concurrent handling of events

--- pkg/events/event_dispatcher.go
+++ pkg/events/event_dispatcher.go
@@ -2,6 +2,7 @@ package events
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
@@ -18,11 +19,13 @@ func NewEventDispatcher() *EventDispatcher {
 
 func (ed *EventDispatcher) Dispatch(event EventInterface) error {
 	if handlers, ok := ed.handlers[event.GetName()]; ok {
+		wg := &sync.WaitGroup{}
 		for _, handler := range handlers {
-			handler.Handle(event)
+			wg.Add(1)
+			go handler.Handle(event, wg)
 		}
+		wg.Wait()
 	}
-
 	return nil
 }
 
